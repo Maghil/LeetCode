34b5c706040733b71e41c8f439d7b662301a7a47
Maghil Vannan <54468795+Maghil@users.noreply.github.com>
2023-11-22 13:47:20 +0530

    Update mine.py

--- Zigzag Conversion/mine.py
+++ Zigzag Conversion/mine.py
@@ -1,18 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        x=0
-        y=0
-        operation=+1
-        result = [[]]
-        print(result)
-        for i in range(0,len(s)):
-            result[0].append(i)
-        print(result)
-
-
-
-if __name__=="__main__":
-    s="adfadfadf"
-    n=2
-    a = Solution()
-    a.convert(s,n)
+        if numRows == 1 or numRows >= len(s):
+            return s
+            
+        x = 0
+        operation = -1
+        result = []
+        for i in range(0, len(s)):
+            if len(result) < numRows:
+                result.append([])
+            result[x].append(s[i])
+            if i % (numRows-1) == 0:
+                operation = operation*-1
+            x = x+operation
+        for i in range(len(result)):
+            result[i] = ''.join(result[i])
+        return ''.join(result)
