72e6ba50e6f3b7c455229e384f12be0a3ddc8c15
Maghil <mmaghil2k@gmail.com>
2024-09-05 11:43:19 +0530

    removed commits

--- Check If N and Its Double Exist/suggested.py
+++ Check If N and Its Double Exist/suggested.py
@@ -2,10 +2,7 @@ class Solution:
     def checkIfExist(self, arr) -> bool:
         seen = set()
         for i in arr:
-            print(f"{i = }")
-            print(f"{seen = }")
-          # if 2 * i in seen or i % 2 == 0 and i // 2 in seen:
-            if 2 * i in seen or i / 2 in seen: # credit to @PeterBohai
+            if 2 * i in seen or i / 2 in seen:
                 return True
             seen.add(i)
         return False
