2078471936f9cf8b0d4a1025638766e5c3caa9c7
Maghil <mmaghil2k@gmail.com>
2024-09-18 00:03:02 +0530

    suggested solution for remove elements

---
+++ Remove Duplicates from Sorted Array/mine2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        index = 1
+        for i in range(0, len(nums)-1):
+            if nums[i] != nums[i+1]:
+                nums[index] = nums[i+1]
+                index += 1
+        return index
+
+if __name__ == "__main__":
+    a = Solution()
+    print(a.removeDuplicates([1, 1, 1, 2, 2, 2]))
+    print(a.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+    print(a.removeDuplicates([-100, 0, 9, 2, 3, 3, 4, 4]))
--- Remove Element/suggested.py
+++ Remove Element/suggested.py
@@ -6,3 +6,9 @@ class Solution:
                 nums[index]=nums[i]
                 index+=1
         return index
+
+if __name__ == "__main__":
+    a = Solution()
+    print(a.removeElement([1, 0, 2, 3, 0, 4, 5, 0], 0))
+    print(a.removeElement([1, 2, 3], 1))
+    print(a.removeElement([3, 2, 2, 3], 3))
