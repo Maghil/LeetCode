8d4a132b2f5eba3cd2b049a896c4e4b91960f121
Maghil Vannan <54468795+Maghil@users.noreply.github.com>
2024-11-20 20:33:13 +0530

    Update mine.py

--- Odd Even Linked List/mine.py
+++ Odd Even Linked List/mine.py
@@ -8,9 +8,6 @@ class LinkedList:
     def __init__(self) -> None:
         self.head = None
 
-    def addAtIndex(self, index) -> None:
-        pass
-
     def addAtHead(self, val):
         node = Node(val)
         if self.head:
@@ -35,15 +32,17 @@ class LinkedList:
 class Solution:
     def oddEvenList(self, head):
         current_node = odd_head = head
-        prev_node = odd_head = next_node = None
+        prev_node = odd_head = next_node = even_head = None
         counter = 1
         while current_node:
             next_node = current_node.next
             if counter == 1:
                 odd_head = current_node
+            elif counter == 2:
+                even_head = current_node
             elif counter % 2 != 0:
                 prev_node.next = current_node.next
-                current_node.next = prev_node
+                current_node.next = even_head
                 odd_head.next = current_node
                 odd_head = current_node
                 current_node = current_node.next
@@ -56,26 +55,7 @@ class Solution:
 if __name__ == "__main__":
     list = LinkedList()
     a = Solution()
-    list.create_list([1, 2, 3, 4, 5])
+    list.create_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     list.print()
     list.head = a.oddEvenList(list.head)
     list.print()
-
-# *1, 2, 3, 4, 5, 6, 7, 8, 9 ,10
-current_node = 1
-odd_head = 1    
-prev_node = None
-next_node = None
-counter = 1
-# 1, *2, 3, 4, 5, 6, 7, 8, 9 ,10
-current_node = 2
-odd_head = 1
-prev_node = 1
-next_node = 3
-counter = 2
-# 1, 2, *3, 4, 5, 6, 7, 8, 9 ,10
-current_node = 3
-odd_head = 1
-prev_node = 2
-counter = 3
-# 1, 3, 2, *4, 5, 6, 7, 8, 9 ,10
