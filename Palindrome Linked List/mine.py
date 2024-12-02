7841af01a3d6a2feb42c9fc3053aa3186593cf8c
Maghil Vannan <54468795+Maghil@users.noreply.github.com>
2024-12-02 23:21:41 +0530

    Update mine.py

--- Palindrome Linked List/mine.py
+++ Palindrome Linked List/mine.py
@@ -1,15 +1,17 @@
 class Node:
-    def __init__(self, value, next=None) -> None:
-        self.value = value
+    def __init__(self, val, next=None) -> None:
+        self.val = val
         self.next = next
 
 
 class LinkedList:
-    def __init__(self) -> None:
+    def __init__(self, vals=None) -> None:
         self.head = None
+        if vals:
+            self.create(vals)
 
-    def addAtHead(self, value):
-        node = Node(value)
+    def addAtHead(self, val):
+        node = Node(val)
         if self.head:
             node.next = self.head
             self.head = node
@@ -20,13 +22,13 @@ class LinkedList:
         current_node = self.head
         output = ""
         while current_node != None:
-            output = output + str(current_node.value) + " "
+            output = output + str(current_node.val) + " "
             current_node = current_node.next
         print(output)
 
-    def create(self, values) -> None:
-        for value in values[::-1]:
-            self.addAtHead(value)
+    def create(self, vals) -> None:
+        for val in vals[::-1]:
+            self.addAtHead(val)
 
 
 class Solution:
@@ -35,24 +37,49 @@ class Solution:
         while fast_pointer and fast_pointer.next:
             fast_pointer = fast_pointer.next.next
             slow_pointer = slow_pointer.next
-            # print(fast_pointer.value)
-        print(slow_pointer.value)
+        slow_pointer = self.reverseList(slow_pointer)
+        fast_pointer = head
+        while slow_pointer:
+            if slow_pointer.val != fast_pointer.val:
+                return False
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next
+        return True
 
-    def reverseList(self, list):
-        previous_node = next_node = None
-        current_node = list.head
+    def reverseList(self, head):
+        previous_node = None
+        current_node = head
         while current_node:
             next_node = current_node.next
             current_node.next = previous_node
-            previous_node = current_node
-            current_node = next_node
+            previous_node, current_node = current_node, next_node
         return previous_node
 
 
 if __name__ == "__main__":
     a = Solution()
-    b = [1, 2, 3, 4, 5, 6, 7, 8]
-    list1 = LinkedList()
-    list1.create(b)
+    b = [1, 2, 3, 4, 4, 3, 2, 1]
+    c = [1, 2, 3, 2, 1]
+    d = [1, 2, 3, 3, 5, 1]
+    e = [1]
+    list1 = LinkedList(b)
+    list2 = LinkedList(c)
+    list3 = LinkedList(d)
+    list4 = LinkedList(e)
     list1.print()
-    a.isPalindrome(list1.head)
+    print(a.isPalindrome(list1.head))
+    list2.print()
+    print(a.isPalindrome(list2.head))
+    list3.print()
+    print(a.isPalindrome(list3.head))
+    list4.print()
+    print(a.isPalindrome(list4.head))
+
+# 1 2 3 4 4 3 2 1 ; 1 1
+# 1 2 3 4 4 3 2 1 ; 2 3
+# 1 2 3 4 4 3 2 1 ; 3 4
+# 1 2 3 4 4 3 2 1 ; 4 2
+
+# 1 2 3 2 1 ; 1 1
+# 1 2 3 2 1 ; 2 3
+# 1 2 3 2 1 ; 3 1
