8e2c7a4ca63bd7cc88d36a40b260800d648f0b4a
Maghil Vannan <54468795+Maghil@users.noreply.github.com>
2025-02-10 10:21:27 +0530

    Create main.java

---
+++ Binary Search/main.java
@@ -0,0 +1,33 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] b = {2, 4, 6, 8, 10};
+        int[] c = {1, 3, 5, 7, 9};
+        int target = 5, target2 = 2, target3 = 2;
+        System.out.println(Solution.search(a, target));
+        System.out.println(Solution.search(b, target2));
+        System.out.println(Solution.search(c, target3));
+    }
+}
+
+class Solution {
+
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                low = mid +1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
